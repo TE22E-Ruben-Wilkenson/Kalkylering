bca08c1b5b191f2ba756e7e87544fc6e7397eb4b
Ruben Wilkenson <ruben.wilkenson@elev.ga.ntig.se>
2023-09-04 09:24:38 +0200

    Gjort uppgift A och B

--- src/Kalkylering.java
+++ src/Kalkylering.java
@@ -1,5 +1,20 @@
 public class Kalkylering {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Uppgift A");
+        int tal1 = 23;
+        int tal2 = 45;
+        int summa;
+        int produkt;
+
+        summa = tal1 + tal2;
+        System.out.println(tal1+"+"+tal2+"="+summa);
+        
+        System.out.println("Uppgift B");
+
+        int tal3 = 23432;
+        int tal4 = 3424;
+        produkt = tal3 * tal4;
+        System.out.println(tal3+"*"+tal4+"="+produkt);
+
     }
 }
